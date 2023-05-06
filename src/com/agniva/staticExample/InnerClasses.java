dcf393edda8f6d9351323f534f8fe3cb272d675c
Agniva Das <agnivadas007@gmail.com>
2023-05-06 17:11:05 +0530

    commit

---
+++ src/com/agniva/staticExample/InnerClasses.java
@@ -0,0 +1,25 @@
+package com.agniva.staticExample;
+
+public class InnerClasses {
+    static class Test {
+        String name;
+        public Test(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test a = new Test("Agni");
+        Test b = new Test("Rahul");
+
+        System.out.println(a);
+
+//        System.out.println(a.name);
+//        System.out.println(b.name);
+    }
+}
